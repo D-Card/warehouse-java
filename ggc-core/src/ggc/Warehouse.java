6a66b5af89ee43b087681b745f5d72a181f94923
D-Card <diogocardoso67@tecnico.ulisboa.pt>
2021-10-08 14:41:21 +0100

    Initial commit

---
+++ ggc-core/src/ggc/Warehouse.java
@@ -0,0 +1,28 @@
+package ggc;
+
+// FIXME import classes (cannot import from pt.tecnico or ggc.app)
+import java.io.*;
+import ggc.exceptions.*;
+
+/**
+ * Class Warehouse implements a warehouse.
+ */
+public class Warehouse implements Serializable {
+
+  /** Serial number for serialization. */
+  private static final long serialVersionUID = 202109192006L;
+
+  // FIXME define attributes
+  // FIXME define contructor(s)
+  // FIXME define methods
+
+  /**
+   * @param txtfile filename to be loaded.
+   * @throws IOException
+   * @throws BadEntryException
+   */
+  void importFile(String txtfile) throws IOException, BadEntryException /* FIXME maybe other exceptions */ {
+    //FIXME implement method
+  }
+
+}
