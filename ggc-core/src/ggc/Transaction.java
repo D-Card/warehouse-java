dae895d9325921ad67692ab702c79e0a9ce25c0b
DCard <diogocardoso67@tecnico.ulisboa.pt>
2021-10-27 11:58:19 +0100

    Add template

---
+++ ggc-core/src/ggc/Transaction.java
@@ -0,0 +1,11 @@
+package ggc;
+
+import java.io.*;
+import java.util.*;
+import ggc.exceptions.*;
+
+public class Transaction implements Serializable {
+
+    private static final long serialVersionUID = 202110270052L;
+
+}
