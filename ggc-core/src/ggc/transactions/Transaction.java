3e66b5bece8de9e6fdd7a4959aed9577735ec849
D-Card <diogocardoso67@tecnico.ulisboa.pt>
2021-11-08 16:35:31 +0000

    Implement Transaction class

--- ggc-core/src/ggc/transactions/Transaction.java
+++ ggc-core/src/ggc/transactions/Transaction.java
@@ -1,11 +1,50 @@
 package ggc.transactions;
-
 import java.io.*;
 import java.util.*;
-import ggc.exceptions.*;
+import ggc.partners.Partner;
+import ggc.products.Product;
 
 public class Transaction implements Serializable {
 
     private static final long serialVersionUID = 202110270052L;
+
+    private int _id;
+    private Partner _partner;
+    private Product _product;
+    private int _amount;
+
+    // Getters
+    public int getId() {
+        return _id;
+    }
+
+    public Partner getPartner() {
+        return _partner;
+    }
+
+    public Product getProduct() {
+        return _product;
+    }
+
+    public int getAmount() {
+        return _amount;
+    }
+
+    //Setters
+    public void setId(int id) {
+        _id = id;
+    }
+
+    public void setPartner(Partner partner) {
+        _partner = partner;
+    }
+
+    public void setProduct(Product product) {
+        _product = product;
+    }   
+
+    public void setAmount(int amount) {
+        _amount = amount;
+    }
 
 }
